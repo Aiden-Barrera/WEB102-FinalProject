3f430b13b3ce1b908dbf8508fb67390cf249569e
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-04-19 13:42:03 -0400

    Added Routes

---
+++ hubpost/src/pages/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+
+    return (
+        <>
+            <header style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "auto",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}>
+                <h1 style={{padding: "0 100px"}}>HubPost</h1>
+                <nav style={{display: "flex", gap: "20px", padding: "0 90px"}}>
+                    <Link to="/" style={{ color: "inherit"}}>
+                        <h2>HOME</h2>
+                    </Link>
+                    <Link to="/createPost" style={{ color: "inherit"}}>
+                        <h2>CREATE POST</h2>
+                    </Link>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
