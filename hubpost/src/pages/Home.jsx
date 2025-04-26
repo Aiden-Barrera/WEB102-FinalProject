f4080b0fa126c3607adcf1289639880e1057c380
Aiden-Barrera <aidenjayce28@gmail.com>
2025-04-26 01:20:14 -0400

    Finished Styling and Filtering post by searching for post title

--- hubpost/src/pages/Home.jsx
+++ hubpost/src/pages/Home.jsx
@@ -3,14 +3,17 @@ import { useLocation } from "react-router-dom";
 import { supabase } from "../Client"
 import {notification} from "antd"
 import PostCard from "../components/PostCard"
-import {Input} from "antd"
+import { LoadingOutlined } from '@ant-design/icons';
+import {Input, Spin, message} from "antd"
 
 const Home = () => {
     const [posts, setPosts] = useState(null)
     const [postLiked, setPostLiked] = useState(false)
     const [api, contextHolder] = notification.useNotification();
+    const [messageApi, messageContextHolder] = message.useMessage();
     const location = useLocation()
     const [userSearched, setUserSearched] = useState("")
+    const [filteredPost, setFilteredPost] = useState(null)
     
 
     useEffect(() => {
@@ -39,22 +42,49 @@ const Home = () => {
         setUserSearched(e.target.value)
     }
 
+    const handleSearch = () => {
+        if (userSearched === "") return setFilteredPost(posts)
+        const filtered = posts.filter((post) => post.post_title.toLowerCase().includes(userSearched.toLowerCase()))
+        if (filtered.length > 0){
+            setFilteredPost(filtered)
+        } else {
+            messageApi.open({
+                type: 'error',
+                content: 'No Post Title Matched!',
+              });
+        }
+    }
+
 
     if (!posts) {
-        return (<div style={{display: "flex", justifyContent: "center", alignItems: "center", fontSize: "48px", fontWeight: "700", width: "100vw"}}>Loading...</div>)
+        return (
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100vw",
+                height: "100vh"
+            }}>
+                <Spin indicator={<LoadingOutlined style={{ fontSize: "64px" }} spin />} />
+                Loading...
+            </div>
+        );
     }
+    
 
     return (
         <>
             {contextHolder}
-            <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "90vh", color: "rgb(239,239,240)"}}>
-                <h1>Welcome to <span style={{fontWeight: "700", color: "rgb(88,185,158)"}}>hubPost</span></h1>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "80vh", color: "rgb(239,239,240)"}}>
+                <h1 style={{margin: "0 0 100px 50px"}}>Welcome to <span style={{fontWeight: "700", color: "rgb(88,185,158)"}}>TrainingFeed</span></h1>
             </div>
             <div style={{display: "flex", flexDirection: "column", gap: "20px", justifyContent: "center", backgroundColor: "rgb(239,239,240)", width: "100vw", height: "auto", padding: "50px"}}>
                 <h1 style={{color: "rgb(26, 26, 30)"}}>Community Workout Post</h1>
                 <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <Input style={{fontSize: "18px", width: "300px", borderRadius: "18px"}} value={userSearched} onChange={handleChange} placeholder="Search by Post Title..." 
-                        prefix={<img src="/searchIcon.svg" alt="Icon" style={{ width: "24px", marginRight: "5px" }} />}
+                    {messageContextHolder}
+                    <Input style={{fontSize: "18px", width: "500px", borderRadius: "18px", marginBottom: "20px"}} value={userSearched} onChange={handleChange} placeholder="Search by Post Title..." 
+                        suffix={<img src="/searchIcon.svg" alt="Icon" style={{ width: "24px", marginLeft: "5px",cursor: "pointer" }} onClick={handleSearch}/>}
                     />
                 </div>
                 <div style={{
@@ -63,7 +93,9 @@ const Home = () => {
                     gap: "30px",
                     justifyContent: "center",
                 }}>
-                    {posts?.map((post, index) => (
+                    {filteredPost ? filteredPost?.map((post, index) => (
+                        <PostCard key={index} id={post.id} name={post.name} title={post.post_title} exercise={post.exercise_name} imgURL={post.imageURL} feedback={post.feedback} likes={post.post_likes} created={post.created_at} postLiked={setPostLiked} fetchPost={fetchPosts}/>
+                    )) : posts?.map((post, index) => (
                         <PostCard key={index} id={post.id} name={post.name} title={post.post_title} exercise={post.exercise_name} imgURL={post.imageURL} feedback={post.feedback} likes={post.post_likes} created={post.created_at} postLiked={setPostLiked} fetchPost={fetchPosts}/>
                     ))}
                 </div>
--- hubpost/src/pages/Navbar.jsx
+++ hubpost/src/pages/Navbar.jsx
@@ -5,16 +5,17 @@ const Navbar = () => {
     return (
         <>
             <header style={{
-                position: "fixed",
+                position: "relative",
                 top: 0,
                 left: 0,
-                width: "100%",
+                width: "100vw",
                 height: "auto",
                 display: "flex",
+                zIndex: 1000,
                 justifyContent: "space-between",
                 alignItems: "center",
             }}>
-                <h1 style={{padding: "0 100px", color: "rgb(88,185,158)"}}>HubPost</h1>
+                <h1 style={{padding: "0 100px", color: "rgb(88,185,158)"}}>TrainingFeed</h1>
                 <nav style={{display: "flex", gap: "20px", padding: "0 50px"}}>
                     <Link to="/" style={{ color: "inherit"}}>
                         <h2>HOME</h2>
